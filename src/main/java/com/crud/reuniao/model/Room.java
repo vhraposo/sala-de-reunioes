5cfb716bb54a6033810d72c7c52ab9ff857843c8
vhraposo <thevhup@gmail.com>
2023-06-03 05:35:47 -0300

    add @Data on model

--- src/main/java/com/crud/reuniao/model/Room.java
+++ src/main/java/com/crud/reuniao/model/Room.java
@@ -1,6 +1,33 @@
 package com.crud.reuniao.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
 
+
+@Entity
+@Table(name = "meetingroom")
+@Data
 public class Room {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String date;
+
+    @Column(nullable = false)
+    private String startHour;
+
+    @Column(nullable = false)
+    private String endHour;
+
 }
